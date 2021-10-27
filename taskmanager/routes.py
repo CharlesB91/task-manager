a04103b2a9c992a911345ce05e14f06e1c69543e
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-27 16:05:36 +0000

    __init__ file updated

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -15,13 +15,12 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
     if request.method == "POST":
         category.category_name = request.form.get("category_name")
-        db.session.commit()
+        db.session.commit() 
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
@@ -30,8 +29,8 @@ def edit_category(category_id):
 def add_category():
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
-        db.session.add(category)
-        db.session.commit()
+        db.session.add(category) 
+        db.session.commit() 
         return redirect(url_for("categories"))
     return render_template("add_category.html")
 
@@ -47,8 +46,8 @@ def add_task():
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
         )
-        db.session.add(task)
-        db.session.commit()
+        db.session.add(task) 
+        db.session.commit()  
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
 
@@ -63,21 +62,23 @@ def edit_task(task_id):
         task.is_urgent = bool(True if request.form.get("is_urgent") else False)
         task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
-        db.session.commit()
+        db.session.commit() 
     return render_template("edit_task.html", task=task, categories=categories)
 
 
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
-    db.session.delete(category)
-    db.session.commit()
+    db.session.delete(category) 
+    db.session.commit() 
     return redirect(url_for("categories"))
 
 
 @app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
-    db.session.delete(task)
-    db.session.commit()
+    db.session.delete(task) 
+    db.session.commit() 
     return redirect(url_for("home"))
+
+
