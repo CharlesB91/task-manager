361b1afc3fa0a75b0961c4965c4735252b49a637
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-27 15:15:12 +0000

    now deleting tasks successfully

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -73,3 +73,11 @@ def delete_category(category_id):
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
