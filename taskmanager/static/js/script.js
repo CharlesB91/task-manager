175eb1422ccb67b097f7484631c75350a50df986
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-27 13:03:25 +0000

    tasks section added

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
+        // datepicker initialization
+        let datepicker = document.querySelectorAll(".datepicker");
+        M.Datepicker.init(datepicker, {
+            format: "dd mmmm, yyyy",
+            i18n: {done: "Select"}
+        });
+    
+        // select initialization
+        let selects = document.querySelectorAll("select");
+        M.FormSelect.init(selects);
+
   });
 
   
